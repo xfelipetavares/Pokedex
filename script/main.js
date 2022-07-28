a37e1bdb1275fc221c74c0229856613e337ae380
Felipe Tavares <xfelipetavares@gmail.com>
2022-07-28 20:44:52 -0300

    funcionando

--- script/main.js
+++ script/main.js
@@ -2,6 +2,9 @@ const pokemonName = document.querySelector(`.pokemon__name`)
 const pokemonNumber = document.querySelector(`.pokemon__number`)
 const pokemonImage = document.querySelector(`.pokemon__image`)
 
+// let buttonPrev = document.getElementsByClassName(`btn-prev`)
+let pokemonId = 1
+
 const fetchPokemon = async (pokemon) => {
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
 
@@ -16,8 +19,9 @@ const renderPokemon = async (pokemon) => {
     pokemonName.innerHTML = data.name
     pokemonNumber.innerHTML = data.id
     pokemonImage.src = data.sprites.versions[`generation-v`][`black-white`].animated.front_default
-    // front_shiny
+    // pokemonImage.src = data.sprites.versions[`generation-v`][`black-white`].animated.front_shiny
 
 }
 
-renderPokemon(`151`)
+
+renderPokemon(`1`)
